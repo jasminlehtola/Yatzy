220744ea06ce49c1626ab93a24b616d2a21d62ce
Jajohl <jajohl@utu.fi>
2026-03-02 12:31:54 +0200

    Test number now renders to the scoreboard

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import players from './data/players'
 
 
 const App = () => {
-  const [players, setPlayers] = useState([]);
-  const [categories, setCategories] = useState([]);
+  //const [players, setPlayers] = useState([]) 
+  //const [categories, setCategories] = useState([])
   const [scores, setScores] = useState({})
 
 
@@ -31,6 +31,7 @@ const App = () => {
           players={players}
           categories={categories}
           scores={scores}
+          setScores={setScores}
         />
       </div>
       <div className="dice">
