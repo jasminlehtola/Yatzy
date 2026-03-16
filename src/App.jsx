4afcf130a1a565214dee282a122a4f170a60c0f1
Janina <jaidap@utu.fi>
2026-03-16 13:33:06 +0200

    Menu buttons & CSS updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 import './index.css'
 import categories from './data/categories'
 import players from './data/players'
 import Dice from './components/die.jsx'
 //import Dice from './components/diceRoll'
 import Scoreboard from './components/scoreboard'
+import Menu from './components/menu.jsx'
 
 const App = () => {
   //const [players, setPlayers] = useState([]) 
@@ -20,18 +21,7 @@ const handleThrow = () => {
 
   return (
     <div className="game">
-      <div className="menu">
-        <button id="start">
-          <p>Start game</p>
-        </button>
-        <button id="end">
-          <p>End game</p>
-        </button>
-        <button id="leaderboard">
-          <p>Leaderboard</p>
-        </button>
-
-      </div>
+      <Menu />
       <div className="gameArea">
         <div className="scoreboard">
           <Scoreboard
