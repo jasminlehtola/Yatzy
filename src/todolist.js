73d864610a94782189dcb6a39400862a2846b6d0
Jajohl <jajohl@utu.fi>
2026-04-01 19:14:35 +0300

    Updated todolist

--- src/todolist.js
+++ src/todolist.js
@@ -6,9 +6,9 @@
 // !!Peli ei ohjaa mitenkään painamaan New game-nappia, vaan sitä voi vaan alkaa pelaamaan myös ilman nimiä.
 // --> Tummennus/disablaus muuhun pelialueeseen, kunnes pelaaja luonut uuden pelin?
 
+// Ohjeet pelin pelaamiseen ja nappien käyttöön
 
 
 // **APP**:
-// auto repeat taustamusiikkiin, vissiin sammuu nyt itsekseen --> vaikuttaa kyllä toimivan?
-// Musiikki kylläkin sammuu silloin kun sivu refreshataan, ja käynnistyy vain Startgame -napilla uudelleen.
-// Tää ei varmaan haittaa
+// Musiikki sammuu silloin kun sivu refreshataan, ja käynnistyy vain Startgame -napilla uudelleen.
+// Tää ei varmaan kyl haittaa
